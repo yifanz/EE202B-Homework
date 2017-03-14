f27b4c998d7ba3dae6c21baa2656bd874b14ca1b
Yi-Fan Zhang <yifanz@yahoo.com>
2017-03-13 20:43:30 -0700

    Debugging answers

--- check_input.py
+++ check_input.py
@@ -1,10 +1,44 @@
 import sys
-from numpy import mean
+import pdb
+from math import sqrt
 from numpy import median
 from numpy import var
 from scipy.stats import skew
 from scipy.stats import kurtosis
 from scipy.stats import pearsonr
+
+def mean(A):
+    s = 0
+    n = len(A)
+    for a in A:
+        s = s + a
+    m = s / n
+    return m
+
+
+def stddev(A):
+    mean_A = mean(A);
+    s = 0
+    for a in A:
+        d = abs(a - mean_A)
+        s += d * d
+    sdev = sqrt(s / len(A))
+    return sdev
+
+
+def covariance(A, B):
+    mean_A = mean(A)
+    mean_B = mean(B)
+    s = 0
+    for x, y in zip(A, B):
+        s += (x - mean_A) * (y - mean_B)
+    covar = s / len(A)
+    return covar
+
+
+def correlation(a, b):
+    corr = covariance(a, b) / (stddev(a) * stddev(b))
+    return corr
 
 
 def calculate_stats(x_chan, y_chan, z_chan):
@@ -25,23 +59,26 @@ def calculate_stats(x_chan, y_chan, z_chan):
             answer.append(max(win_x))
             answer.append(mean(win_x))
             answer.append(var(win_x))
-            answer.append(skew(win_x))
+            answer.append(skew(win_x, bias=False))
             answer.append(kurtosis(win_x, fisher=False))
             answer.append(min(win_y))
             answer.append(max(win_y))
             answer.append(mean(win_y))
             answer.append(var(win_y))
-            answer.append(skew(win_y))
+            answer.append(skew(win_y, bias=False))
             answer.append(kurtosis(win_y, fisher=False))
             answer.append(min(win_z))
             answer.append(max(win_z))
             answer.append(mean(win_z))
             answer.append(var(win_z))
-            answer.append(skew(win_z))
+            answer.append(skew(win_z, bias=False))
             answer.append(kurtosis(win_z, fisher=False))
-            answer.append(pearsonr(win_x,win_y)[0])
-            answer.append(pearsonr(win_x,win_z)[0])
-            answer.append(pearsonr(win_y,win_z)[0])
+            answer.append(correlation(win_x, win_y))
+            answer.append(correlation(win_x, win_z))
+            answer.append(correlation(win_y, win_z))
+            #answer.append(pearsonr(win_x,win_y)[0])
+            #answer.append(pearsonr(win_x,win_z)[0])
+            #answer.append(pearsonr(win_y,win_z)[0])
 
     # report median at the end only
     answer.append(median(x_chan))
